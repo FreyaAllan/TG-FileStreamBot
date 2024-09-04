0e159dbe65afafd4fb3d3c91820a18d40a348ddd
Rplay <88985252+Renishrplay@users.noreply.github.com>
2024-09-04 22:55:48 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ git clone https://github.com/EverythingSuckz/TG-FileStreamBot
 cd /TG-FileStreamBot
 go build ./cmd/fsb/
 Rename-Item -LiteralPath ".\fsb.sample.env" -NewName ".\fsb.env"
-notepad fsb.env
+#notepad fsb.env
 # (add your environment variables, see the next section for more info)
 .\fsb run
 #pip3 install -U -r requirements.txt
