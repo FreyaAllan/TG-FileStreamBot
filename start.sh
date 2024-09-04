237bcdd6e982cccee35cb9e27ea0d35289f5eb71
Rplay <88985252+Renishrplay@users.noreply.github.com>
2024-09-04 22:54:25 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+echo "Cloning main Repository"
+git clone https://github.com/EverythingSuckz/TG-FileStreamBot
+cd /TG-FileStreamBot
+go build ./cmd/fsb/
+Rename-Item -LiteralPath ".\fsb.sample.env" -NewName ".\fsb.env"
+notepad fsb.env
+# (add your environment variables, see the next section for more info)
+.\fsb run
+#pip3 install -U -r requirements.txt
+#echo "Starting Bot...."
+#python3 bot.py
